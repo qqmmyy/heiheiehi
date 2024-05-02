a6af7ba57f1057367b2550010d9d1a081ead9f02
qqmmyy <164297647+qqmmyy@users.noreply.github.com>
2024-05-02 15:40:06 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,27 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: index
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/气候纵览',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component:气候纵览
+  },
+   {
+    path: '/',
+    name: 'home',
+    component: index
+  },
+   {
+    path: '/人流出行',
+    component: 人流出行
   }
 ]
 
